040253d43f0d73c300f1b9cda783e0f59719a745
Fred Pan <fredpan@usc.edu>
2023-04-12 01:07:40 +0000

    Initial commit for hw5

---
+++ schedwork.cpp
@@ -0,0 +1,103 @@
+#ifndef RECCHECK
+#include <set>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+// add or remove necessary headers as you please
+
+#endif
+
+#include "schedwork.h"
+
+using namespace std;
+
+// a constant that can be used to indicate an INVALID 
+// worker ID if that is useful to your implementation.
+// Feel free to not use or delete.
+static const Worker_T INVALID_ID = (unsigned int)-1;
+
+// Add prototypes for any helper functions here
+bool schedule_helper(unsigned int day, unsigned int slot, const size_t dailyNeed, const size_t maxShifts, const AvailabilityMatrix& avail, DailySchedule& sched, vector<unsigned int>& shifts);
+
+// Add your implementation of schedule() and other helper functions here
+bool schedule(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched
+)
+{
+    if(avail.size() == 0U){
+        return false;
+    }
+    sched.clear();
+    // Add your code below
+    int nworkers = avail[0].size(); // number of workers
+    // initializations
+    vector<unsigned int> shifts(nworkers);
+    for(unsigned int i = 0; i < shifts.size(); i++) {
+        shifts[i] = 0; // how many shifts a worker has already worked
+    }
+
+    sched = DailySchedule(avail.size(), vector<Worker_T>(dailyNeed));
+    for(unsigned int i = 0; i < avail.size(); i++) {
+      for(unsigned int j = 0; j < dailyNeed; j++) {
+        sched[i][j] = -1;
+      }
+    }
+
+    // call the helper function
+    bool ans =  schedule_helper(0, 0, dailyNeed, maxShifts, avail, sched, shifts);
+    return ans;   
+}
+
+// backtracking helper function
+bool schedule_helper(unsigned int day, unsigned int slot, const size_t dailyNeed, const size_t maxShifts, const AvailabilityMatrix& avail, DailySchedule& sched, vector<unsigned int>& shifts)
+{
+    // finishes assigning
+    if(day >= sched.size()) {
+        return true;
+    }
+      //std::cout << "new iteration" << std::endl;
+    // assign workers
+    for(unsigned int i = 0; i < avail[day].size(); i++) {
+        // check for availability
+
+        if(avail[day][i] == 1 && shifts[i] < maxShifts) {
+            sched[day][slot] = i;
+            shifts[i]++;
+        
+        // check if we need to remove this worker from available workers
+        
+        // change the day if necessary
+        bool answer = false;
+        if(slot == dailyNeed - 1) {
+          answer = schedule_helper(day + 1, 0, dailyNeed, maxShifts, avail, sched, shifts);
+        }
+        // recursively call the function for the next day
+        else{
+          answer = schedule_helper(day, slot + 1, dailyNeed, maxShifts, avail, sched, shifts);
+        }
+        if(answer == true)
+          return true;
+        // backtracking
+        shifts[i]--;
+        sched[day][i] = -1;
+        //std::cout << "val at sched[][] " << day << " " << i << " " << sched[day][i] << std::endl;
+      }
+    }
+    // cannot assign any workers, return false
+    return false;
+}
+
+
+
+
+
+
+
+
+
---
+++ wordle.cpp
@@ -0,0 +1,74 @@
+#ifndef RECCHECK
+// For debugging
+#include <iostream>
+// For std::remove
+#include <algorithm> 
+#include <map>
+#include <set>
+#endif
+
+#include "wordle.h"
+#include "dict-eng.h"
+using namespace std;
+
+// Add prototypes of helper functions here
+void wordle_helper(std::string in, std::string& floating, unsigned int cindex, std::set<std::string>& words, const std::set<std::string>& dictionary);
+
+// Definition of primary wordle function
+std::set<std::string> wordle(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict)
+{
+    // Add your code here
+    std::set<std::string> output;
+    std::string in_copy = in;
+    std::string floating_copy = floating;
+    wordle_helper(in_copy, floating_copy, 0, output, dict);
+    return output;
+}
+
+// Define any helper functions here
+void wordle_helper(std::string in, std::string& floating, unsigned int cindex, std::set<std::string>& words, const std::set<std::string>& dictionary)
+{
+  if(cindex == in.size()) {
+    if(floating.size() == 0 && dictionary.find(in) != dictionary.end()) {
+      words.insert(in);
+      return;
+    }
+  }//--------------------------------------------------
+  else if(in[cindex] == '-') {
+    int count = 0;
+    //----------
+    for(unsigned int i = cindex + 1; i < in.size(); i++) {
+      if(in[i] != '-') {count++;}
+    }
+    //----------
+    unsigned int left = in.size() - cindex;
+    std::set<char> used;
+    //----------
+    for(unsigned int i = 0; i < floating.size(); i++) {
+      if(used.find(floating[i]) != used.end()) {continue;}
+      in[cindex] = floating[i];
+      std::string temp = floating;
+      temp.erase(i, 1);
+      wordle_helper(in, temp, cindex + 1, words, dictionary);
+      used.insert(floating[i]);
+      in[cindex] = '-';
+    }
+    //----------
+    if(left > floating.size() + count) {
+      for(char letter = 'a'; letter <= 'z'; letter++) {
+        if(used.find(letter) != used.end()) {continue;}
+        in[cindex] = letter;
+        wordle_helper(in, floating, cindex + 1, words, dictionary);
+        in[cindex] = '-';
+      }
+    }
+  }//---------------------------------------------
+  else {
+    wordle_helper(in, floating, cindex + 1, words, dictionary);
+  }
+}
+
+
